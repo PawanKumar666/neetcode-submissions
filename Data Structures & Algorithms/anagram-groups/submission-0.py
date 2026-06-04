a4d0fe3ebf3aa87ef02dddb5dd17b65ddc9e55b3
Pawankumar <55320127+PawanKumar666@users.noreply.github.com>
2026-06-04 22:32:24 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        theMap = {}
+        for string in strs:
+            sorted_str = ''.join(sorted(string))
+            if sorted_str not in theMap:
+                theMap[sorted_str] = [string]
+            else:
+                theMap[sorted_str].append(string)
+        return list(theMap.values())
